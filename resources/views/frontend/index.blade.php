b9a0ab4342bec118201ba2b5e1ebbc974e94196c
Deepak Tripathi <111495527+deepak-tripathi165@users.noreply.github.com>
2023-06-25 23:31:45 +0530

    Home Page Slide Dynamic

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.main_master')
 @section('main')
     <!-- banner-area -->
-    <section class="banner">
+    <!-- <section class="banner">
     <div class="container custom-container">
     <div class="row align-items-center justify-content-center justify-content-lg-between">
     <div class="col-lg-6 order-0 order-lg-2">
@@ -24,7 +24,8 @@
     <div class="banner__video">
     <a href="https://www.youtube.com/watch?v=XHOmBV4js_E" class="popup-video"><i class="fas fa-play"></i></a>
 
-    </section>
+    </section> -->
+    @include('frontend.home_all.home_slide')
     <!-- banner-area-end -->
 
     <!-- about-area -->
@@ -34,32 +35,32 @@
     <div class="col-lg-6">
     <ul class="about__icons__wrap">
     <li>
-    <img class="light" src="assets/img/icons/xd_light.png" alt="XD">
-    <img class="dark" src="assets/img/icons/xd.png" alt="XD">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/xd_light.png') }}" alt="XD">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/xd.png') }}" alt="XD">
     </li>
     <li>
-    <img class="light" src="assets/img/icons/skeatch_light.png" alt="Skeatch">
-    <img class="dark" src="assets/img/icons/skeatch.png" alt="Skeatch">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/skeatch_light.png') }}" alt="Skeatch">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/skeatch.png') }}" alt="Skeatch">
     </li>
     <li>
-    <img class="light" src="assets/img/icons/illustrator_light.png" alt="Illustrator">
-    <img class="dark" src="assets/img/icons/illustrator.png" alt="Illustrator">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/illustrator_light.png') }}" alt="Illustrator">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/illustrator.png') }}" alt="Illustrator">
     </li>
     <li>
-    <img class="light" src="assets/img/icons/hotjar_light.png" alt="Hotjar">
-    <img class="dark" src="assets/img/icons/hotjar.png" alt="Hotjar">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/hotjar_light.png') }}" alt="Hotjar">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/hotjar.png') }}" alt="Hotjar">
     </li>
     <li>
-    <img class="light" src="assets/img/icons/invision_light.png" alt="Invision">
-    <img class="dark" src="assets/img/icons/invision.png" alt="Invision">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/invision_light.png') }}" alt="Invision">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/invision.png') }}" alt="Invision">
     </li>
     <li>
-    <img class="light" src="assets/img/icons/photoshop_light.png" alt="Photoshop">
-    <img class="dark" src="assets/img/icons/photoshop.png" alt="Photoshop">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/photoshop_light.png') }}" alt="Photoshop">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/photoshop.png') }}" alt="Photoshop">
     </li>
     <li>
-    <img class="light" src="assets/img/icons/figma_light.png" alt="Figma">
-    <img class="dark" src="assets/img/icons/figma.png" alt="Figma">
+    <img class="light" src="{{ asset('frontend/assets/img/icons/figma_light.png') }}" alt="Figma">
+    <img class="dark" src="{{ asset('frontend/assets/img/icons/figma.png') }}" alt="Figma">
     </li>
     </ul>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::controller(AdminController::class)->group(function(){
 
 Route::controller(HomeSlideController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
 
 
 });
